aa15b9134fa524c2bc237a2f5291c96921eef774
samuel-hawkins <samueledwardhawkins@gmail.com>
2020-10-27 13:22:23 -0400

    Add in completed field for models

--- app/models/question.js
+++ app/models/question.js
@@ -14,6 +14,10 @@ const questionSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
+  },
+  answered: {
+    type: Boolean,
+    default: false
   }
 }, {
   timestamps: true
